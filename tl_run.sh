42498f026df4f76cbc0fbf6efdf5c06ef08057f3
zoeleeee <zhuzby@outlook.com>
2020-01-07 19:48:15 +0000

    .

--- tl_run.sh
+++ tl_run.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=$PYTHONPATH:$PWD
-python FastAutoAugment/train.py -c confs/pyramid272_cifar100_2_tl.yaml --aug fa_reduced_cifar10 --dataset permutated_cifar100 --save cifar100_pyramid272_top1_11.74.pth --nb-labels 2 --classifier-id 0  --dataroot /home/zhuzby/data
+CUDA_VISIBLE_DEVICES=0 python FastAutoAugment/train.py -c confs/pyramid272_cifar100_2_tl.yaml --aug fa_reduced_cifar10 --dataset permutated_cifar100 --save cifar100_pyramid272_top1_11.74.pth --nb-labels 2 --classifier-id 0  --dataroot /home/zhuzby/data
