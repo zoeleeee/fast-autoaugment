5e714b913c44a5e4000d2db122172bc4e2b417cf
zoeleeee <zhuzby@outlook.com>
2020-01-10 11:02:01 +0000

    .

---
+++ foolbox_test.py
@@ -0,0 +1,27 @@
+import foolbox
+import numpy as np
+import torchvision.models as models
+
+# instantiate model (supports PyTorch, Keras, TensorFlow (Graph and Eager), JAX, MXNet and many more)
+model = models.resnet18(pretrained=True).eval()
+preprocessing = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], axis=-3)
+fmodel = foolbox.models.PyTorchModel(model, bounds=(0, 1), num_classes=1000, preprocessing=preprocessing)
+
+# get a batch of images and labels and print the accuracy
+images, labels = foolbox.utils.samples(dataset='imagenet', batchsize=16, data_format='channels_first', bounds=(0, 1))
+print(np.mean(fmodel.forward(images).argmax(axis=-1) == labels))
+# -> 0.9375
+
+# apply the attack
+attack = foolbox.attacks.FGSM(fmodel)
+adversarials = attack(images, labels)
+# if the i'th image is misclassfied without a perturbation, then adversarials[i] will be the same as images[i]
+# if the attack fails to find an adversarial for the i'th image, then adversarials[i] will all be np.nan
+
+# Foolbox guarantees that all returned adversarials are in fact in adversarials
+print(np.mean(fmodel.forward(adversarials).argmax(axis=-1) == labels))
+print([a.adversarial_class for a in adversarials])
+# -> 0.0
+print(np.sum(adversarials[0]-images[0]))
+print(adversarials.shape)
+print(np.max(adversarials), np.min(adversarials))
