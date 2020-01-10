68c890d55322c6fc7ca133d3f0e3ad9a1ed7f772
zoeleeee <zhuzby@outlook.com>
2020-01-10 15:46:51 +0000

    .

--- cws.py
+++ cws.py
@@ -9,7 +9,7 @@ import sys
 import os
 
 def get_data(path = '/home/zhuzby/data'):
-	# _CIFAR_MEAN, _CIFAR_STD = (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+	_CIFAR_MEAN, _CIFAR_STD = (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
 	transform_test = transforms.Compose([
 			transforms.ToTensor(),
 			# transforms.Normalize(_CIFAR_MEAN, _CIFAR_STD),
@@ -36,7 +36,7 @@ def target_model(save_path):
 if __name__ == '__main__':
 	_ = C(sys.argv[-1])
 	model = target_model(sys.argv[-2])
-	preprocessing = dict(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010], axis=-3)
+	preprocessing = dict(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
 	fmodel = foolbox.models.PyTorchModel(model, bounds=(-3, 3), num_classes=100, preprocessing=preprocessing)
 
 	normal_correct = 0
@@ -50,28 +50,28 @@ if __name__ == '__main__':
 		# print(np.max(images), np.min(images))
 		# images, labels = foolbox.utils.samples(dataset='cifar100', batchsize=64, data_format='channels_first', bounds=(0, 1))
 		normal_correct += np.sum(fmodel.forward(images).argmax(axis=-1) == label)
-		attack = foolbox.attacks.CarliniWagnerL2Attack(fmodel, distance=foolbox.distances.MeanSquaredDistance)
-		adversarials = attack(images, label, unpack=False)
-		# adv_imgs += [a for a in adversarials]
-		# adversarial_classes = fmodel.forward(adversarials).argmax(axis=-1)
-		adv_imgs += [a.perturbed for a in adversarials]
-		adversarial_classes = [a.adversarial_class for a in adversarials]
-		adv_correct += np.mean(adversarial_classes == label)  # will always be 0.0
-		if labels == []:
-			labels = label
-		else:
-			labels = np.hstack((labels,label))
-		# labels += label
-	np.save('cifar100_advs.npy', adv_imgs)
-	np.save('cifar100_labels.npy', labels)
-	print(np.max(adv_imgs), np.min(adv_imgs))
+	# 	attack = foolbox.attacks.CarliniWagnerL2Attack(fmodel, distance=foolbox.distances.MeanSquaredDistance)
+	# 	adversarials = attack(images, label, unpack=False)
+	# 	# adv_imgs += [a for a in adversarials]
+	# 	# adversarial_classes = fmodel.forward(adversarials).argmax(axis=-1)
+	# 	adv_imgs += [a.perturbed for a in adversarials]
+	# 	adversarial_classes = [a.adversarial_class for a in adversarials]
+	# 	adv_correct += np.mean(adversarial_classes == label)  # will always be 0.0
+	# 	if labels == []:
+	# 		labels = label
+	# 	else:
+	# 		labels = np.hstack((labels,label))
+	# 	# labels += label
+	# np.save('cifar100_advs.npy', adv_imgs)
+	# np.save('cifar100_labels.npy', labels)
+	# print(np.max(adv_imgs), np.min(adv_imgs))
 	print('normal acc:', normal_correct / len(loader.dataset))
-	print('adversarial acc:', adv_correct / len(loader.dataset))
+	# print('adversarial acc:', adv_correct / len(loader.dataset))
 	
 
 	# The `Adversarial` objects also provide a `distance` attribute. Note that the distances
 	# can be 0 (misclassified without perturbation) and inf (attack failed).
-	distances = np.asarray([a.distance.value for a in adversarials])
-	print("{:.1e}, {:.1e}, {:.1e}".format(distances.min(), np.median(distances), distances.max()))
-	print("{} of {} attacks failed".format(sum(adv.distance.value == np.inf for adv in adversarials), len(adversarials)))
-	print("{} of {} inputs misclassified without perturbation".format(sum(adv.distance.value == 0 for adv in adversarials), len(adversarials)))
+	# distances = np.asarray([a.distance.value for a in adversarials])
+	# print("{:.1e}, {:.1e}, {:.1e}".format(distances.min(), np.median(distances), distances.max()))
+	# print("{} of {} attacks failed".format(sum(adv.distance.value == np.inf for adv in adversarials), len(adversarials)))
+	# print("{} of {} inputs misclassified without perturbation".format(sum(adv.distance.value == 0 for adv in adversarials), len(adversarials)))
