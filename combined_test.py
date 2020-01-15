9558e72ee62f2cbe55c34e529cd46a2fcdc4a205
zoeleeee <zhuzby@outlook.com>
2020-01-15 11:10:11 +0000

    .

--- combined_test.py
+++ combined_test.py
@@ -36,7 +36,7 @@ def check_combined(imgs, label_path, nb_labels, idx):
 	valids = np.ones(imgs.shape[0])
 	model_dir = 'models'
 	files = os.listdir(model_dir)
-	entries = {int(file.split('_')[-5]): os.path.join(model_dir, file) if len(file.split('_')) > 9 else continue for file in files}
+	entries = {int(file.split('_')[-5]) if len(file.split('_')) > 9 else -1 : os.path.join(model_dir, file)  for file in files}
 	print(entries)
 	nb_files = len(files)
 
