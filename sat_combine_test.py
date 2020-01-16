ee1fc89e8726f71ec4abbc34959b7574e9c919ae
zoeleeee <zhuzby@outlook.com>
2020-01-16 17:39:07 +0000

    .

--- sat_combine_test.py
+++ sat_combine_test.py
@@ -6,9 +6,9 @@ def evaluation(nb_res):
 	samples = np.load('res_48_{}.npy'.format(nb_res)).T[10:40]
 	labels = np.load('cifar100_labels_{}.npy'.format(nb_res))
 	orders = np.random.permutation(samples.shape[0])
-	ex_labels = [[rep[i][j] for j in labels] for i in orders]
-	valid = samples == ex_labels
-	sep_valid = [[np.max(val[j:j+3]) for j in np.arange(valid.shape[-1])[::3]] for val in valid]
+	ex_labels = np.array([[rep[i][j] for j in labels] for i in orders])
+	valid = (samples == ex_labels)
+	sep_valid = np.array([[np.max(val[j:j+3]) for j in np.arange(valid.shape[-1])[::3]] for val in valid])
 	valid = np.sum(sep_valid, axis=-1)
 	print(sep_valid.shape, valid.shape)
 	print('acc:', np.sum(valid==10))
