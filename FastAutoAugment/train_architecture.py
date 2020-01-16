0ddd8a9adb5d643b99038f6a37ac8b2484ad39e8
zoeleeee <zhuzby@outlook.com>
2020-01-16 11:18:49 +0000

    .

--- FastAutoAugment/train_architecture.py
+++ FastAutoAugment/train_architecture.py
@@ -173,11 +173,12 @@ def train_and_eval(tag, dataroot, test_ratio=0.0, cv_fold=0, reporter=None, metr
             logger.info('checkpoint epoch@%d' % data['epoch'])
             if not isinstance(model, DataParallel):
                 # only for Pyramid cifar100
-                weights = {k.replace('module.', 'module.model.'): v for k, v in data[key].items()}
-                weights['module.model.fc.weight'] = torch.rand_like(model.state_dict()['module.model.fc.weight'])
-                weights['module.model.fc.bias'] = torch.rand_like(model.state_dict()['module.model.fc.bias'])
-                weights['module.fc.weight'] = torch.rand_like(model.state_dict()['module.fc.weight'])
-                weights['module.fc.bias'] = torch.rand_like(model.state_dict()['module.fc.bias'])
+                weights = {k if 'module.model' in k else 'module.model.'+k: v for k, v in data[key].items()}
+                # weights = {k.replace('module.', 'module.model.'): v for k, v in data[key].items()}
+                # weights['module.model.fc.weight'] = torch.rand_like(model.state_dict()['module.model.fc.weight'])
+                # weights['module.model.fc.bias'] = torch.rand_like(model.state_dict()['module.model.fc.bias'])
+                # weights['module.fc.weight'] = torch.rand_like(model.state_dict()['module.fc.weight'])
+                # weights['module.fc.bias'] = torch.rand_like(model.state_dict()['module.fc.bias'])
                 model.load_state_dict(weights)
             else:
                 weights = {k if 'module.model' in k else 'module.model.'+k: v for k, v in data[key].items()}
