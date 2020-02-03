69b3e72a5567f2b562ac465353389cd667665ebf
zoeleeee <zhuzby@outlook.com>
2020-02-03 12:36:13 +0000

    .

--- combined_test.py
+++ combined_test.py
@@ -122,7 +122,7 @@ def check_classifier(imgs, label_path, path='cifar100_pyramid272_30outputs_500ep
 		_predict = np.array([[1 if u >= 0.5 else 0 for u in v] for v in _predicted])
 		_labels = np.array([reps[int(i)] for i in _label])
 		if len(preds) == 0:
-			preds = _predicted
+			preds = _predict
 			valid = (np.sum(_predict-_labels) == 0)
 			score = np.mean(np.abs(_predict-_labels), axis=-1)
 		else:
